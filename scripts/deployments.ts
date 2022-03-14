263ea7ac9f55a3a3bf735776cd6b4624a1bec930
Kenneth Hurley <ken@geniusventures.io>
2022-03-14 12:36:36 -0700

    Refactor deployment code for being able to upgrade facets and verify all contracts

---
+++ scripts/deployments.ts
@@ -0,0 +1,35 @@
+
+export const deployments = {
+  mumbai: {
+    DiamondAddress: '0x127e47aba094a9a87d084a3a93732909ff031419',
+    FacetCutAddress: '0xe30526ba2128f8cc4d05b013c65a623478b2ad80',
+    DeployerAddress: '0x55f36651B5B61B8286305740fA86AD996FC8bDc9',
+    FacetAddresses: [
+      '0xf95EB593dDd7806beCC4419bD67371244187EA05',
+      '0xad36d09B567FD6eC5B580804902A728F074F6df4',
+      '0x611449746E13035471B90901d9Fb08abd0707346',
+      '0xcEddE1968e33DC8F182e94EEE8738436867dd65A',
+      '0xD3745fecBF088da6214CC472aa2Fd629f76eCb1B',
+      '0x25b3f982924f6d31eb8354a414C929D6107aFB8a'
+    ]
+  },
+  polygon: {
+    DiamondAddress: '',
+    FacetCutAddress: '',
+    DeployerAddress: '',
+    FacetAddresses: []
+  },
+  localhost: {
+    DiamondAddress: '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512',
+    FacetCutAddress: '',
+    DeployerAddress: '',
+    FacetAddresses: [
+      '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+      '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9',
+      '0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9',
+      '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707',
+      '0x0165878A594ca255338adfa4d48449f69242Eb8F',
+      '0xa513E6E4b8f2a923D98304ec87F64353C4D5C853'
+    ]
+  }
+};
---
+++ scripts/upgrade.ts
@@ -0,0 +1,57 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { debug } from "debug";
+// @ts-ignore
+import { ethers } from "hardhat";
+import {di, IDeployInfo} from "../scripts/common";
+import { GeniusDiamond } from "../typechain-types/GeniusDiamond";
+import { DiamondCutFacet } from "../typechain-types/DiamondCutFacet";
+import { deployments } from "../scripts/deployments";
+import { deployGNUSDiamondFacets } from "./deploy";
+const log: debug.Debugger = debug("GNUSUpgrade:log");
+import hre from "hardhat";
+import fs from "fs";
+import util from "util";
+
+// @ts-ignore
+log.color = "158";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  if (require.main === module) {
+    log.enabled = true;
+    //const network = await ethers.getDefaultProvider().getNetwork();
+    const networkName = hre.network.name;
+    if (networkName in deployments) {
+      const deployInfo = deployments[networkName as keyof typeof deployments] as IDeployInfo;
+      const diamondAddress: string = deployInfo.DiamondAddress;
+      const Diamond = await ethers.getContractFactory("hardhat-diamond-abi/GeniusDiamond.sol:GeniusDiamond");
+      di.gnusDiamond = Diamond.attach(diamondAddress) as GeniusDiamond;
+      di.diamondCutFacet = await ethers.getContractAt("DiamondCutFacet", di.gnusDiamond.address) as DiamondCutFacet;
+      // TODO: check ABI for contract or time updated and only deploy updated contracts
+      deployInfo.FacetAddresses = [];
+      await deployGNUSDiamondFacets(deployInfo);
+      log(`Contract address deployed is ${di.gnusDiamond.address}`);
+      fs.writeFileSync('scripts/deployments.ts', `\nexport const deployments = ${util.inspect(deployments)};\n`, "utf8")
+    } else {
+      log(`No deployments found to attach to for ${networkName}, aborting.`);
+    }
+
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
