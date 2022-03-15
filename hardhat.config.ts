19c5d6817e4216c0fbcd689662590ef8f70c972b
Kenneth Hurley <ken@geniusventures.io>
2022-03-15 15:36:57 -0700

    changing default rpc connection

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -71,7 +71,7 @@ const config: HardhatUserConfig = {
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com/v1/12dd792f8cba3178b2b151e0ba6b80ab5d046b79",
+      url: "https://matic-testnet-archive-rpc.bwarelabs.com",
       accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
     polygon: {
