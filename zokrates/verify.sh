71d732d6c57d6cd245eb617fd1f0db4f05b8f276
Kenneth Hurley <ken@geniusventures.io>
2022-07-14 08:56:25 -0700

    WIP, testing, zok file is debug commented

---
+++ zokrates/verify.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# generate/compute witness file for inputs
+../../ZoKrates/target/release/zokrates compute-witness --abi -i SGVerifier.zkbin -o SGVerifier.witness --stdin <SGVerifier.abi.json
+
+../../ZoKrates/target/release/zokrates generate-proof -i SGVerifier.zkbin -p SGProving.key -w SGVerifier.witness -j SGProof.json -s gm17 -b ark
+
+../../ZoKrates/target/release/zokrates verify -v SGVerifier.key -j SGProof.json -s gm17 -b ark
+
