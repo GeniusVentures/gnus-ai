2f0063b5855f86435c68b11686ee6448d83a2906
Kenneth Hurley <ken@geniusventures.io>
2022-07-23 18:25:39 -0700

    Still WIP, but using newest ZoKrates, which If statement isn't normal, but at least I can log now

--- zokrates/genSol.sh
+++ zokrates/genSol.sh
@@ -1,6 +1,10 @@
 # !/bin/bash
 
-../../ZoKrates/target/release/zokrates compile -i SGVerifier.zok -o SGVerifier.zkbin --stdlib-path=../../ZoKrates/zokrates_stdlib/stdlib/
+../../ZoKrates/target/release/zokrates compile --debug -i SGVerifier.zok -o SGVerifier.zkbin --stdlib-path=../../ZoKrates/zokrates_stdlib/stdlib/
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  exit $retVal
+fi
 
 # generate universal setup?
 # generate verifier and proving keys
@@ -9,4 +13,4 @@
 # generate verifier and proving keys
 ../../ZoKrates/target/release/zokrates setup -i SGVerifier.zkbin -v SGVerifier.key -p SGProving.key -s gm17 -b ark
 
-../../ZoKrates/target/release/zokrates export-verifier -i SGVerifier.key -o SGVerifier.sol -s gm17
+../../ZoKrates/target/release/zokrates export-verifier -i SGVerifier.key -o SGVerifier.sol
