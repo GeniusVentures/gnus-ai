3aa01fc0757f254856b8173dfe148d78f315bc1d
Kenneth Hurley <ken@geniusventures.io>
2022-07-23 18:25:39 -0700

    Still WIP, but using newest ZoKrates, which If statement isn't normal, but at least I can log now

--- zokrates/verify.sh
+++ zokrates/verify.sh
@@ -5,5 +5,5 @@
 
 ../../ZoKrates/target/release/zokrates generate-proof -i SGVerifier.zkbin -p SGProving.key -w SGVerifier.witness -j SGProof.json -s gm17 -b ark
 
-../../ZoKrates/target/release/zokrates verify -v SGVerifier.key -j SGProof.json -s gm17 -b ark
+../../ZoKrates/target/release/zokrates verify -v SGVerifier.key -j SGProof.json -b ark
 
