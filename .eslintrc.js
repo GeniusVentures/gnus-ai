c844d7684532e184da615f2561224596d2953aad
ruymaster <mecury1984@outlook.com>
2024-01-24 16:10:25 +0200

    fix deploying facets with external libs and add prettier formatter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,25 @@
 module.exports = {
-    env: {
-        browser: false,
-        es2021: true,
-        mocha: true,
-        node: true
-    },
-    plugins: ["@typescript-eslint"],
-    extends: [
-        "standard",
-        "plugin:node/recommended",
-        "plugin:import/typescript"
+  env: {
+    browser: false,
+    es2021: true,
+    mocha: true,
+    node: true,
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["standard", "plugin:node/recommended", "plugin:import/typescript"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    "node/no-unsupported-features/es-syntax": [
+      "error",
+      { ignores: ["modules"] },
     ],
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-        ecmaVersion: 12
-    },
-    rules: {
-        "node/no-unsupported-features/es-syntax": [
-            "error",
-            { ignores: ["modules"] }
-        ],
-        quotes: "off",
-        indent: ["warn", 4],
-        semi: "off"
-    }
-}
+    quotes: "off",
+    indent: ["warn", 2],
+    semi: "off",
+    "comma-dangle": 0,
+    "space-before-function-paren": 0
+  },
+};
