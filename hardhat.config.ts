179e7a99163e8fad7e589613eb06961b1d9005cf
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2024-12-18 18:24:02 -0500

    updated yarn>betty, +npm packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,13 @@
 import * as dotenv from 'dotenv';
 
 import { HardhatUserConfig, task } from 'hardhat/config';
-import '@nomiclabs/hardhat-etherscan';
-import '@nomiclabs/hardhat-waffle';
+import "@nomicfoundation/hardhat-toolbox";
 import 'hardhat-diamond-abi';
 import 'hardhat-abi-exporter';
 import '@typechain/hardhat';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
-import '@nomiclabs/hardhat-ethers';
+import '@nomicfoundation/hardhat-ethers';
 import '@nomiclabs/hardhat-web3';
 
 dotenv.config();
